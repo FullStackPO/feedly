272d12e23d9337802764e7e0f000021f7af58fdb
Harsh Arya <harsharya8946@gmail.com>
2026-06-08 22:57:39 +0530

    send image from server to imageKit

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -1,5 +1,19 @@
+const imageKit = require('@imagekit/nodejs')
+const { toFile } = require('@imagekit/nodejs')
+
+const imagekit = new imageKit({
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY
+})
+
 async function postController(req,res){
     console.log(req.body, req.file);
+
+    const file = await imagekit.files.upload({
+        file : await toFile(Buffer.from(req.file.buffer), 'file'),
+        fileName : 'Test'
+    })
+
+    res.send(file)
 }
 
 module.exports = postController
