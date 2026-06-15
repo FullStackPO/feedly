3667daa2be8259695f134f7668b1cfecdf4aa2c7
Harsh Arya <harsharya8946@gmail.com>
2026-06-15 23:34:30 +0530

    imp conditions in followUsers()

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,9 +1,37 @@
 const followModel = require('../models/follow.model')
+const userModel = require('../models/user.model')
 
 async function followUsers(req, res){
 
     const userFollower = req.user.username
     const userFollowee = req.params.username
+
+    if(userFollowee == userFollower){
+        return res.status(400).json({
+            message : 'you cannot follow yourself.'
+        })
+    }
+
+    const isFolloweeExist = await userModel.findOne({
+        username : userFollowee
+    })
+
+    if(!isFolloweeExist){
+        return res.status(404).json({
+            message : `user is not exist`
+        })
+    }
+
+    const isAlreadyFollowing = await followModel.findOne({
+        follower : userFollower,
+        followee : userFollowee
+    })
+
+    if(isAlreadyFollowing){
+        return res.status(200).json({
+            message : `you already follow the ${userFollowee}.`
+        })
+    }
 
     const followRecord = await followModel.create({
         follower : userFollower,
