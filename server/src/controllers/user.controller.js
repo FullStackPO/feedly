b29b02f4108eb3bd00debdbbc94b22e90ce76c3b
Harsh Arya <harsharya8946@gmail.com>
2026-06-16 17:21:24 +0530

    unfollow feature

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 const followModel = require('../models/follow.model')
 const userModel = require('../models/user.model')
 
+//follow feature
+
 async function followUsers(req, res){
 
     const userFollower = req.user.username
@@ -45,4 +47,29 @@ async function followUsers(req, res){
 
 }
 
-module.exports = followUsers
+//unfollow feature
+
+async function unfollowUsers(req,res){
+
+    const userFollower = req.user.username
+    const userFollowee = req.params.username
+
+    const isUserFollowing = await followModel.findOne({
+        followee : userFollowee,
+        follower : userFollower
+    })
+
+    if(!isUserFollowing){
+        return res.status(200).json({
+            message : `You are not following ${userFollowee}.`
+        })
+    }
+
+    await followModel.findByIdAndDelete(isUserFollowing._id)
+
+    res.status(200).json({
+        message : 'You unfollow the user'
+    })
+}
+
+module.exports = { followUsers , unfollowUsers }
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const identifyUser = require('../middlewares/auth.middleware')
-const followUser = require('../controllers/user.controller')
+const { followUsers , unfollowUsers } = require('../controllers/user.controller')
 const userRouter = express.Router()
 
-userRouter.post('/follow/:username', identifyUser , followUser)
+userRouter.post('/follow/:username', identifyUser , followUsers)
+
+userRouter.post('/unfollow/:username', identifyUser , unfollowUsers)
 
 module.exports = userRouter
